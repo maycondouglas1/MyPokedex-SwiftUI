17c45c81967b31e4b4ba6b483cb9be8b4443a56c
Maycon Douglas <mayconmrqs2001@gmail.com>
2024-07-22 11:46:26 -0300

    feat: login or signup view

--- MyPokedex/LoginAndSignUp/View/LoginAndSignUpView.swift
+++ MyPokedex/LoginAndSignUp/View/LoginAndSignUpView.swift
@@ -9,7 +9,98 @@ import SwiftUI
 
 struct LoginAndSignUpView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(spacing: 35) {
+                jumpButton
+                Spacer()
+                trainerImages
+                titleAndDescription
+                buttons
+            }
+            .navigationBarBackButtonHidden()
+        .padding()
+        }
+    }
+    
+    var jumpButton: some View {
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+            HStack {
+                Text("Pular")
+                    .font(Font.custom("Poppins-Medium", size: 18))
+                    .foregroundStyle(Color("Primary"))
+                Image(systemName: "arrow.right")
+                    .foregroundStyle(Color("Primary"))
+            }
+            .frame(maxWidth: .infinity, alignment: .trailing)
+        })
+        
+    }
+    
+    var trainerImages: some View {
+        ZStack {
+            Image("trainerHatsune")
+                .offset(x: -60)
+                .background {
+                    Image("shadowHatsune")
+                        .offset(x: -70, y: 100)
+                }
+            Image("trainerHilbert")
+                .offset(x: 60, y: -10)
+                .background {
+                    Image("shadowHilbert")
+                        .offset(x: 70, y: 110)
+                }
+        }
+    }
+    
+    var titleAndDescription: some View {
+        VStack(spacing: 16) {
+            Text("Está pronto para essa aventura?")
+                .font(Font.custom("Poppins-Medium", size: 26))
+                .multilineTextAlignment(.center)
+                .foregroundStyle(Color("Primary"))
+                .fixedSize(horizontal: false , vertical: true)
+            
+            Text("Basta criar uma conta e começar a explorar o mundo dos Pokémon hoje!")
+                .font(Font.custom("Poppins-Regular", size: 14))
+                .multilineTextAlignment(.center)
+                .foregroundStyle(Color("Secondary"))
+                .fixedSize(horizontal: false , vertical: true)
+        }
+    }
+    
+    var createAccountButton: some View {
+        NavigationLink {
+            SignUpView()
+        } label: {
+            Rectangle()
+                .frame(height: 58)
+                .clipShape(.capsule)
+                .foregroundStyle(Color("Blue"))
+                .overlay {
+                    Text("Criar conta")
+                        .font(Font.custom("Poppins-Bold", size: 18))
+                        .foregroundStyle(Color(.white))
+                }
+        }
+    }
+    
+    var haveAccountButton: some View {
+        NavigationLink {
+            Text("Teste")
+        } label: {
+            Text("Já tenho uma conta")
+                .font(Font.custom("Poppins-Bold", size: 18))
+                .foregroundStyle(Color("Blue"))
+                .frame(height: 58)
+        }
+    }
+    
+    var buttons: some View {
+        VStack {
+            createAccountButton
+            haveAccountButton
+        }
     }
 }
 
---
+++ MyPokedex/LoginAndSignUp/View/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  MyPokedex
+//
+//  Created by Maycon Douglas on 22/07/24.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
